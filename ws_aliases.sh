b6ab8d22c4c685404fd06679a74399de85544ff6
Alyzen Jeraj <alyzenjeraj@gmail.com>
2024-10-29 16:18:12 -0400

    Updated aliases

--- ws_aliases.sh
+++ ws_aliases.sh
@@ -4,7 +4,10 @@ ROS2_CONTAINER_ID=""
 # Function to run the Docker container interactively with a mounted volume
 function ws-start {
     local workspace_dir=$(pwd)
-    ROS2_CONTAINER_ID=$(docker run -d -it -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent -v $workspace_dir:/nre_luci_ws/src -p 8765:8765 luci.jfrog.io/ros2-sdk-docker-local/luci-ros2-sdk:latest)
+    ROS2_CONTAINER_ID=$(docker run -d -it -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent -v $workspace_dir/src:/root/nre_luci_ws/src -p 8765:8765 luci.jfrog.io/ros2-sdk-docker-local/luci-ros2-sdk:latest)
+    docker exec "$ROS2_CONTAINER_ID" apt install python3-pip -y
+    docker exec "$ROS2_CONTAINER_ID" pip install setuptools==58.2.0
+    echo "Updated setuptools package to allow colcon build to run without warnings"
     echo "Started container with ID: $ROS2_CONTAINER_ID"
 
 }
@@ -12,7 +15,7 @@ function ws-start {
 # Function to attach to the running container using
 function ws-exec {    
     if [ -n "$ROS2_CONTAINER_ID" ]; then
-        docker exec -it "$ROS2_CONTAINER_ID" bash
+        docker exec -it -w /root/nre_luci_ws "$ROS2_CONTAINER_ID" bash 
     else
         echo "No container is currently running. Start a container first."
     fi    

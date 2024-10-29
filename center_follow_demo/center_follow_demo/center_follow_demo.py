af49b3de0588d71add97c41073befe7ef0edb5f5
Alyzen Jeraj <alyzenjeraj@gmail.com>
2024-10-28 20:56:51 -0400

    Updated node code

--- center_follow_demo/center_follow_demo/center_follow_demo.py
+++ center_follow_demo/center_follow_demo/center_follow_demo.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import rclpy
 from rclpy.node import Node
 from luci_messages.msg import LuciJoystick
 from luci_messages.msg import LuciImu
-from #IMPORT THE OCCUPANCY GRID
+from sensor_msgs.msg import PointCloud2
+from sensor_msgs_py.point_cloud2 import read_points
 
 class CenterDrive(Node):
     def __init__(self):
@@ -12,4 +14,18 @@ class CenterDrive(Node):
         # TODO: What name do I publish to?
         self.joystick_publisher = self.create_publisher(LuciJoystick, '/', 10)
 
-        self.lidar_subscriber = self.create_subscription()
+        self.collision_subscriber = self.create_subscription(PointCloud2, "/collision", self.collision_callback, 10)
+
+
+    def collision_callback(self, msg):
+        points = read_points(msg, field_names=("x", "y", "z"), skip_nans=True)
+        for point in points:
+            x, y, z = point[:3]
+            print(f"Point: x={x}, y={y}, z={z}")
+
+if __name__ == "__main__":
+    rclpy.init(args=args)
+    center_drive = CenterDrive()
+    rclpy.spin(center_drive)
+    center_drive.destroy_node()
+    rclpy.shutdown()
